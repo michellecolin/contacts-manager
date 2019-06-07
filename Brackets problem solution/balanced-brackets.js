530b9a46f54eb61c607d0234b8b2e3f3b54a7bd0
Michelle Colin <michelle.c.colin@gmail.com>
2019-06-07 12:24:59 -0300

    Allow user to verify as many texts as he wants Accept Upper and lower case response

--- Brackets problem solution/balanced-brackets.js
+++ Brackets problem solution/balanced-brackets.js
@@ -50,25 +50,57 @@ prompt.get(prompt_attributes, function (err, result) {
 
 const inquirer = require('inquirer');
 
-var questions = [{
+const questionText = [{
   type: 'input',
   name: 'string',
   message: "Please, input the text you want to verify:",
 }];
 
 console.log('\nWelcome to the "balanced brackets" program! Here you can verify if your brackets are balanced :)\n');
+askForUserText();
 
-inquirer.prompt(questions).then(answers => {
-  console.log('\nVerifying...\n');
-  const isBalanced = isBalancedParenthesis(answers['string']);
-  if (isBalanced) {
-    console.log('Your text brackets are balanced! Yay :)');
-  } else {
-    console.log('Your text brackets are NOT balanced! :(');
-  }
-});
 
-const isBalancedParenthesis = (str) => {
+//----- functions ----
+
+function askForUserText() {
+  inquirer.prompt(questionText).then(answers => {
+    console.log('\nVerifying...');
+  
+    const isBalanced = isBalancedParenthesis(answers['string']);
+  
+    if (isBalanced) {
+      console.log('\nYour text brackets are balanced! Yay :)\n');
+    } else {
+      console.log('\nYour text brackets are NOT balanced! :(\n');
+    }
+  
+    const questionVeirfyAgain = [{
+      type: 'input',
+      name: 'verifyAgain',
+      message: "Do you want to verify another text? (y/n)",
+    }];
+  
+    inquirer.prompt(questionVeirfyAgain).then(answers => {
+      let answer = answers['verifyAgain'].toLowerCase(); 
+
+      if (answer === 'y') {
+        console.log('\n');
+        askForUserText();
+      } else if (answer === 'n') {
+        console.log('\nThanks for using the "balanced brackets" program, see you soon!\n');
+      } else {
+        questionNotValid = [{
+          type: 'input',
+          name: 'notValid',
+          message: 'This answer was not valid, please type "y" for yes and "n" for no.\n',
+        }];
+      }
+    });
+  
+  });
+}
+
+function isBalancedParenthesis(str) {
   return !str.split('').reduce((uptoPrevChar, thisChar) => {
       if(thisChar === '(' || thisChar === '{' || thisChar === '[' ) {
           return ++uptoPrevChar;
